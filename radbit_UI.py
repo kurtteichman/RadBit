09a589326051c8bc1e96d801ba0fa85ec2158d3a
eddie-benim <eb34472@my.utexas.edu>
2025-06-18 14:40:29 -0400

    Update and rename radbit_UI to radbit_UI.py

--- radbit_UI.py
+++ radbit_UI.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from triage_logic import triage_and_get_support_info
+from agents import set_default_openai_key
+
+set_default_openai_key(st.secrets["OPENAI_API_KEY"])
 
 st.set_page_config(page_title="Radiology Support Triage", layout="centered")
 st.title("Radiologist Support Request Portal")
